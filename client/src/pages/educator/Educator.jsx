84d092688b9a337bdf0faa27a1b5872d061951e8
Kavita Mahato <kavitamahato0835@gmail.com>
2026-01-12 12:30:24 +0530

    Setting up Routes

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>
+      <h1>Educator</h1>
+      <div>
+        {<Outlet/>}
+      </div>
+    </div>
   )
 }
 
