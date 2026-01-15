4a85b069fa455654a230b14d1cf233c526bd621d
Kavita Mahato <kavitamahato0835@gmail.com>
2026-01-15 19:09:56 +0530

    NavBar, Hero section, search courses

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
+
+  const isCourseListPage = location.pathname.includes('/course-list');
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+
   return (
-    <div>Navbar</div>
+    <div className={`w-full flex items-center justify-between px-4 py-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+      <img src={assets.logo} alt='Logo' className='w-28 lg:w-32 cursor-pointer'/>
+      <div className='hidden md:flex items-center gap-5 text-gray-500'>
+        <div className='flex items-center gap-5'>
+          { user && <>
+            <button>Become Educator</button>
+          | <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+          }
+        </div>
+        { user ? <UserButton/> :
+          <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer'>Log In</button>}
+      </div>
+        {/* Mobile view */}
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+          { user && <>
+            <button>Become Educator</button>
+          | <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+          }
+        </div>
+        { user ? <UserButton/> : <button onClick={() => openSignIn()}><img src={assets.user_icon} alt='User Icon' className='w-6 h-6 cursor-pointer'/></button>}
+      </div>
+    </div>
   )
 }
 
--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import { assets } from '../../assets/assets'
 
-const SearchBar = () => {
+const SearchBar = ({data}) => {
+
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : " ");
+
+  // To handle search
+  const onSearchHandller = (e) => {
+    e.preventDefault();
+    navigate("/course-list/" + input);
+  };
+
   return (
-    <div>Search Bar</div>
+    <form onSubmit={onSearchHandller} className='max-w-xl w-full h-14 flex items-center bg-white rounded-lg shadow-md border border-gray-200 focus-within:ring-2 focus-within:ring-blue-200 transition'>
+      <img src={assets.search_icon} alt="Search Icon" className='md:auto w-10 px-3'/>
+      <input  onChange={e => setInput(e.target.value)}
+      type="text" placeholder='Search courses...' className='w-full h-full outline-none text-gray-500/80'/>
+      <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>Search</button>
+    </form>
   )
 }
 
