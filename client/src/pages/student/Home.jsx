ab1fc3a5d6d88a96034c246ed4040dd3600bc2e4
Kavita Mahato <kavitamahato0835@gmail.com>
2026-01-16 23:54:48 +0530

    Call to action

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../../components/student/Hero"
 import Companies from "../../components/student/Companies"
 import CoursesSection from "../../components/student/CoursesSection"
 import TestimonialSection from "../../components/student/TestimonialSection"
+import CallToAction from "../../components/student/CallToAction"
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Companies/>
       <CoursesSection/>
       <TestimonialSection/>
+      <CallToAction/>
     </div>
   )
 }
