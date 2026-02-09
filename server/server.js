35e708474bd4ba9f974a897b190fde6c25e18330
Kavita Mahato <kavitamahato0835@gmail.com>
2026-02-09 10:11:12 +0530

    app.listen removed

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
 app.post('/clerk', express.json(), clerkWebHooks);
 
 // Start the server
+/*
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); */
