59d2e883bb755860ea8eba6bedd2be04a69d2dd1
Kavita Mahato <kavitamahato0835@gmail.com>
2026-05-21 09:30:59 +0530

    Update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { dummyCourses } from '../assets/assets';
 import humanizeDuration from 'humanize-duration';
 import { useAuth, useUser } from "@clerk/clerk-react";
 import axios from 'axios';
